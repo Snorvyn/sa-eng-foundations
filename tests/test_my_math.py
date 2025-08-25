6b0b8107cca2c74fe03725984ac5439c114a8a47
Snorvyn <davidh@superannotate.com>
2025-08-25 16:39:24 +0400

    feat(math): add subtract() and tests (#2)

--- tests/test_my_math.py
+++ tests/test_my_math.py
@@ -1,5 +1,9 @@
-from sa_foundations.my_math import add
+from sa_foundations.my_math import add, subtract
 
 
 def test_add() -> None:
     assert add(2, 3) == 5
+
+
+def test_subtract() -> None:
+    assert subtract(10, 3) == 7
